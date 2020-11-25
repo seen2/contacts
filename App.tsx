1a15ec72f8e88da7f0046b8c2583bdb06456959a
seen2 <tiwarietc@gmail.com>
2020-11-25 16:54:56 +0530

    genrated randome contacts and displayed it

--- App.tsx
+++ App.tsx
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import getContact from "./src/contacts";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={{ fontSize: 20, color: "white" }}>Contacts</Text>
+      <ScrollView>
+        {getContact(100).map((contact) => (
+          <Text
+            key={contact.key}
+            style={{ color: "teal" }}
+          >{`${contact.firstName} ${contact.lastName}: \n${contact.phone}\n`}</Text>
+        ))}
+      </ScrollView>
+      <StatusBar style="light" backgroundColor="black" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#111111",
+    padding:10
   },
 });
